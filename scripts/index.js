0c3734a1de3094736830f2938f9722d19faf0322
Svetlana Shildyaeva <arnab@yandex.ru>
2024-03-24 20:35:06 +0300

    mesto-project-ff v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardItem, delFunction) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = cardItem['link'];
+    cardElement.querySelector('.card__image').alt = cardItem['name'];
+    cardElement.querySelector('.card__title').textContent = cardItem['name'];
+    
+    const delButton = cardElement.querySelector('.card__delete-button');
+
+    delButton.addEventListener('click', () => {
+        delFunction(delButton);
+    });
+    
+    cardList.append(cardElement);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard (event){
+    const cardItemDel = event.closest('.card');
+
+    cardItemDel.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(card => addCard(card, deleteCard));
+
